b7c0a1c308472bd4b585af7ed3d479149bcffebf
Wulle91 <mvuckovi91@gmail.com>
2023-06-09 12:23:06 +0000

    Add posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from .models import Post
 
-class ProfileSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = ReadOnlyField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
