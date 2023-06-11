5d98ed990b99b60da1154b8757dec2e4377e118f
Wulle91 <mvuckovi91@gmail.com>
2023-06-11 13:54:31 +0000

    Likes

---
+++ likes/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
