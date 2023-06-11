4e6e528e30b16417e08976d2eb46113d13ae716f
Wulle91 <mvuckovi91@gmail.com>
2023-06-11 22:52:28 +0000

    Folowers

--- profiles/views.py
+++ profiles/views.py
@@ -6,33 +6,44 @@ from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
 class ProfileList(APIView):
+    """
+    List all profiles
+    No Create view (post method), as profile creation handled by django signals
+    """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
-    
-    
+
+
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_objects(self, pk):
+
+    def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
             self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
-        
+
     def get(self, request, pk):
-        profile = self.get_objects(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, context={'request': request}
+        )
         return Response(serializer.data)
-    
+
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
