db7db0ed977fccdadec7b6309c163760a57be14f
Wulle91 <mvuckovi91@gmail.com>
2023-06-24 10:08:47 +0000

    cors allowed origins 3

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -38,13 +38,20 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN'),
          'https://3000-wulle91-momentss-jgf77wfe8mu.ws-eu100.gitpod.io',
-         'https://8000-wulle91-djangoreact-5643y6znzg1.ws-eu100.gitpod.io'
+         'https://8000-wulle91-djangoreact-5643y6znzg1.ws-eu100.gitpod.io',
+         'https://pythondjangorest-f503576af96c.herokuapp.com',
+         'https://momentos-4387c0084266.herokuapp.com/'
      ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
+    
+CORS_ALLOW_HEADERS = [
+    'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Methods',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENICATION_CLASSES': [(
