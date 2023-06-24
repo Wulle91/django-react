e0b660ff56224dd8ade198c4be4079bf84e60f6f
Wulle91 <mvuckovi91@gmail.com>
2023-06-24 08:44:03 +0000

    cors

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -79,6 +79,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+CORS_ALLOW_HEADERS = [
+    'Access-Control-Allow-Origin',
+]
+
 
 
 # Application definition
