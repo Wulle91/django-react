959c505e6806a536295506a2ce3b8021d4f2547e
Wulle91 <mvuckovi91@gmail.com>
2023-06-25 20:57:56 +0000

    cors allowed origins delete

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE, SESSION_COOKIE_SAMESITE, SESSION_COOKIE_SECURE,
+    CSRF_COOKIE_SAMESITE, CSRF_COOKIE_SECURE,
+)
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
