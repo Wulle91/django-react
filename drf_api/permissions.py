e85d89e79309641fd622247aee653ba1be62aad9
Wulle91 <mvuckovi91@gmail.com>
2023-06-25 20:05:38 +0000

    correct settings 2

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -1,8 +1,22 @@
 from rest_framework import permissions
 
-
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        
+        # Adjust cookie settings based on user authentication
+        if request.user.is_authenticated:
+            # User is logged in
+            SESSION_COOKIE_SAMESITE = 'Lax'
+            SESSION_COOKIE_SECURE = False
+            CSRF_COOKIE_SAMESITE = 'Lax'
+            CSRF_COOKIE_SECURE = False
+        else:
+            # User is not logged in
+            SESSION_COOKIE_SAMESITE = 'None'
+            SESSION_COOKIE_SECURE = True
+            CSRF_COOKIE_SAMESITE = 'None'
+            CSRF_COOKIE_SECURE = True
+
         return obj.owner == request.user
