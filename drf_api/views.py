a5b421589b67a090d8799f411d30cae91b3d8eb0
Wulle91 <mvuckovi91@gmail.com>
2023-06-25 20:54:25 +0000

    cors allowed origins delete

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,74 +1,29 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE, SESSION_COOKIE_SAMESITE, SESSION_COOKIE_SECURE,
-    CSRF_COOKIE_SAMESITE, CSRF_COOKIE_SECURE,
-)
 
-@api_view()
-def root_route(request):
-    return Response({
-        "message": "Welcome to my DRF API"
-    })
-    
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def logout_route(request):
     response = Response()
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key='JWT_AUTH_COOKIE_NAME',
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite='None',
+        secure=True,
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key='JWT_AUTH_REFRESH_COOKIE_NAME',
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite='None',
+        secure=True,
     )
-    response.set_cookie(
-        key=SESSION_COOKIE_SAMESITE,
-        value='Lax',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    response.set_cookie(
-        key=SESSION_COOKIE_SECURE,
-        value='',
-        httponly=False,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    response.set_cookie(
-        key=CSRF_COOKIE_SAMESITE,
-        value='Lax',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    response.set_cookie(
-        key=CSRF_COOKIE_SECURE,
-        value='Lax',
-        httponly=False,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
+    
     return response
+
