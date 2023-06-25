33b59d0d80463efc771cf7b9401de45efbe974d1
Wulle91 <mvuckovi91@gmail.com>
2023-06-25 21:00:44 +0000

    cors allowed origins delete

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,20 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE, SESSION_COOKIE_SAMESITE, SESSION_COOKIE_SECURE,
-    CSRF_COOKIE_SAMESITE, CSRF_COOKIE_SECURE,
-)
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def logout_route(request):
     response = Response()
-    response.delete_cookie(SESSION_COOKIE_SAMESITE)
-    response.delete_cookie(SESSION_COOKIE_SECURE)
-    response.delete_cookie(CSRF_COOKIE_SAMESITE)
-    response.delete_cookie(CSRF_COOKIE_SECURE)
     response.set_cookie(
         key='JWT_AUTH_COOKIE_NAME',
         value='',
